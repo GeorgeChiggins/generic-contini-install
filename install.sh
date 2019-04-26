59844602040eede64079f28b1bfd94b5afa7172a
George Higgins <32547380+GeorgeChiggins@users.noreply.github.com>
2019-04-26 13:48:25 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+wait 5
+brew install terraform
+wait 5 
+brew install awscli
+wait 5
+brew install kubernetes-cli
